f156761b518ab3fb96cc7a3f0945cbf685e25426
DisBiscuit <daviwhit482@proton.me>
2025-08-15 20:33:06 +0300

    Task Manager changed to class-based design with improved error handling.

--- taskmanager.py
+++ taskmanager.py
@@ -1,69 +1,88 @@
-# functions
+class TaskManager():
+    def __init__(self):
+        self.tasks = []
 
-def addTask():
-    task = ''
-    while task != 'x':
-        task = str(input("Enter task to add (enter 'x' to return to menu): "))
-        if task != 'x':
-            taskList.append(task)
+    # add task using user input, if user enters 'x', exits method
 
-# delete task using pop so when an element above it is removed all elements shift up
+    def addTask(self):
+        task = ''
+        while task != 'x':
+            task = str(input("Enter task to add (enter 'x' to return to menu): "))
+            if task != 'x':
+                self.tasks.append(task)
+  
+    # delete task using pop so when an element above it is removed all elements shift up
 
-def delTask():
-    for index, element in enumerate(taskList, start=1):
-        print(f"{index}. {element}")
-    taskNum = ''
-    while taskNum != 'x':
-        taskNum = input("Select a task to delete (enter 'x' to return to menu): ")
-        if taskNum != 'x':
-            num = int(taskNum)
-            taskList.pop(num - 1)
-
-# allows users to re-write whatever is in that numbered place
-
-def editTask():
-    for index, element in enumerate(taskList, start=1):
-        print(f"{index}. {element}")
-    taskNum = int(input("Select a task to edit: "))
-    newTask = str(input("Enter the edited task: "))
-    taskList[taskNum - 1] = newTask
-
-# view task using enumerate so each task is numbered before it
-
-def viewTask():
-    print("Current tasks: ")
-    for index, element in enumerate(taskList, start=1):
-        print(f"{index}. {element}")
-
-# saving to file using write so that it overrides whatever is currently in that file
-
-def saveFile():
-    file_name = str(input("Enter the name of the file you want to save to: "))
-    try:
-        with open(file_name, "w") as file:
-            for task in taskList:
-                file.write(task + "\n")
-        print("Tasks successfully saved to file!")
-    except:
-        print("Invalid file name.")
-
-# using try except so that if user enters an invalid file name, program doesn't break
-
-def loadFile():
-    file_name = str(input("Enter the name of the file you want to load from: "))
-    try:
-        with open(file_name, "r") as file:
-            for line in file:
-                taskList.append(line.strip())
-            print("Tasks have been loaded successfully.")
-    except FileNotFoundError:
-        print("That file doesn't exist.")
+    def delTask(self):
+        if not self.tasks:
+            print('No tasks to delete.')
+            return
+        self.viewTask()
+        taskNum = ''
+        while taskNum != 'x':
+            taskNum = input("Select a task to delete (enter 'x' to return to menu): ")
+            if taskNum != 'x':
+                try:
+                    num = int(taskNum)
+                    self.tasks.pop(num - 1)
+                except (ValueError, IndexError):
+                    print("Invalid task number.")
         
+    # allows users to re-write whatever is in that numbered place
+
+    def editTask(self):
+        if not self.tasks:
+            print('No tasks to edit.')
+            return
+        self.viewTask()
+        try:
+            taskNum = int(input("Select a task to edit: "))
+            newTask = input("Enter the edited task: ")
+            self.tasks[taskNum - 1] = newTask
+        except (ValueError, IndexError):
+            print("Invalid task number.")
+
+    # view task using enumerate so each task is numbered before it
+
+    def viewTask(self):
+        if not self.tasks:
+            print('No tasks to show.')
+            return
+        print("Current tasks: ")
+        for index, element in enumerate(self.tasks, start=1):
+            print(f"{index}. {element}")
+
+    # saving to file using write so that it overrides whatever is currently in that file
+
+    def saveFile(self):
+        file_name = str(input("Enter the name of the file you want to save to: "))
+        try:
+            with open(file_name, "w") as file:
+                for task in self.tasks:
+                    file.write(task + "\n")
+            print("tasks successfully saved to file!")
+        except:
+            print("Invalid file name.")
+
+    # using try except so that if user enters an invalid file name, program doesn't break
+
+    def loadFile(self):
+        self.tasks.clear()
+        file_name = str(input("Enter the name of the file you want to load from: "))
+        try:
+            with open(file_name, "r") as file:
+                for line in file:
+                    self.tasks.append(line.strip())
+                print("Tasks have been loaded successfully.")
+        except FileNotFoundError:
+            print("That file doesn't exist.")
+
 # main
 
-taskList = []
-
 def Main():
+
+    manager = TaskManager()
+
     while True:
         print("Welcome to your Task Manager!")
         print("------------------------------")
@@ -82,20 +101,20 @@ def Main():
             continue
 
         if choice == 1:
-            addTask()
+            manager.addTask()
         elif choice == 2:
-            delTask()
+            manager.delTask()
         elif choice == 3:
-            editTask()
+            manager.editTask()
         elif choice == 4:
-            viewTask()
+            manager.viewTask()
         elif choice == 5:
-            saveFile()
+            manager.saveFile()
         elif choice == 6:
-            loadFile()
+            manager.loadFile()
         elif choice == 7:
             print("Goodbye!")
-            quit()
+            break
         else:
             False
             print("Invalid choice, try again.")
